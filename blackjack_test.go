33e6f9e12afcc343d61b2e11da1bd16b21500ae3
Iain Diamond <iain.diamond@gmail.com>
2019-02-24 14:45:35 +0000

    test winning hands

--- blackjack_test.go
+++ blackjack_test.go
@@ -7,25 +7,28 @@ import (
 )
 
 var (
-	ten = deck.Card{Suit: deck.Hearts, Value: deck.Ten}
-	ace = deck.Card{Suit: deck.Spades, Value: deck.Ace}
+	ace   = deck.Card{Suit: deck.Spades, Value: deck.Ace}
+	two   = deck.Card{Suit: deck.Spades, Value: deck.Two}
+	three = deck.Card{Suit: deck.Spades, Value: deck.Three}
+	four  = deck.Card{Suit: deck.Spades, Value: deck.Four}
+	five  = deck.Card{Suit: deck.Spades, Value: deck.Five}
+	six   = deck.Card{Suit: deck.Spades, Value: deck.Six}
+	seven = deck.Card{Suit: deck.Spades, Value: deck.Seven}
+	eight = deck.Card{Suit: deck.Spades, Value: deck.Eight}
+	nine  = deck.Card{Suit: deck.Spades, Value: deck.Nine}
+	ten   = deck.Card{Suit: deck.Spades, Value: deck.Ten}
+	jack  = deck.Card{Suit: deck.Spades, Value: deck.Jack}
+	queen = deck.Card{Suit: deck.Spades, Value: deck.Queen}
+	king  = deck.Card{Suit: deck.Spades, Value: deck.King}
 )
 
 func TestScoring(t *testing.T) {
 
-	player1 := Player{
-		name: "TestPlayer",
-	}
-
-	dealer := Dealer{
-		Player: Player{
-			name: "Dealer",
-		},
-	}
+	player1 := Player{name: "TestPlayer"}
+	dealer := Dealer{Player: Player{name: "Dealer"}}
 
 	t.Run("a player can score cards", func(t *testing.T) {
-		card1 := deck.Card{Suit: deck.Hearts, Value: deck.Four}
-		player1.cards = []deck.Card{card1, ten}
+		player1.cards = []deck.Card{four, ten}
 		wanted := 14
 		got := player1.score()
 
@@ -35,8 +38,7 @@ func TestScoring(t *testing.T) {
 	})
 
 	t.Run("a player has a low ace card", func(t *testing.T) {
-		card1 := deck.Card{Suit: deck.Spades, Value: deck.Six}
-		player1.cards = []deck.Card{card1, ten, ace}
+		player1.cards = []deck.Card{six, ten, ace}
 		wanted := 17
 		got := player1.score()
 
@@ -56,9 +58,8 @@ func TestScoring(t *testing.T) {
 	})
 
 	t.Run("the dealer can score cards", func(t *testing.T) {
-		card1 := deck.Card{Suit: deck.Hearts, Value: deck.King}
 		dealer.hiddenCard = ace
-		dealer.cards = []deck.Card{card1}
+		dealer.cards = []deck.Card{king}
 
 		wanted := 21
 		got := dealer.score()
@@ -69,8 +70,7 @@ func TestScoring(t *testing.T) {
 	})
 
 	t.Run("the dealer has a low ace card", func(t *testing.T) {
-		card1 := deck.Card{Suit: deck.Spades, Value: deck.Six}
-		dealer.cards = []deck.Card{card1, ten}
+		dealer.cards = []deck.Card{six, ten}
 		dealer.hiddenCard = ace
 		wanted := 17
 		got := dealer.score()
@@ -81,8 +81,7 @@ func TestScoring(t *testing.T) {
 	})
 
 	t.Run("the dealer has a high ace card", func(t *testing.T) {
-		card1 := deck.Card{Suit: deck.Clubs, Value: deck.Nine}
-		dealer.cards = []deck.Card{card1}
+		dealer.cards = []deck.Card{nine}
 		dealer.hiddenCard = ace
 		wanted := 20
 		got := dealer.score()
@@ -91,5 +90,52 @@ func TestScoring(t *testing.T) {
 			t.Errorf("got %d, wanted %d", got, wanted)
 		}
 	})
+}
 
+func TestWinner(t *testing.T) {
+
+	player1 := Player{name: "TestPlayer"}
+	dealer := Dealer{Player: Player{name: "Dealer"}}
+
+	t.Run("player wins", func(t *testing.T) {
+		player1.cards = []deck.Card{nine, ten}
+		dealer.cards = []deck.Card{eight}
+		dealer.hiddenCard = ten
+		game := []Participant{&player1, &dealer}
+		wanted := "TestPlayer"
+
+		got := decideWinner(game)
+
+		if got != wanted {
+			t.Errorf("got %q, wanted %q", got, wanted)
+		}
+	})
+
+	t.Run("dealer wins", func(t *testing.T) {
+		player1.cards = []deck.Card{nine, ten}
+		dealer.cards = []deck.Card{ten}
+		dealer.hiddenCard = ace
+		game := []Participant{&player1, &dealer}
+		wanted := "Dealer"
+
+		got := decideWinner(game)
+
+		if got != wanted {
+			t.Errorf("got %q, wanted %q", got, wanted)
+		}
+	})
+
+	t.Run("game is drawn", func(t *testing.T) {
+		player1.cards = []deck.Card{nine, ten}
+		dealer.cards = []deck.Card{ten}
+		dealer.hiddenCard = nine
+		game := []Participant{&player1, &dealer}
+		wanted := "Draw"
+
+		got := decideWinner(game)
+
+		if got != wanted {
+			t.Errorf("got %q, wanted %q", got, wanted)
+		}
+	})
 }
